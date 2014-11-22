8bcfe0818263265e5c7691ae92a48e408b965063
z33l <z33l@users.noreply.github.com>
2014-11-22 20:42:58 +0000

    Add basic debug logger

--- bot.js
+++ bot.js
@@ -5,6 +5,7 @@ var config = {
 }
 
 var irc = require('irc');
+var util = require('util');
 
 var bot = new irc.Client(config.server, config.botName, {
   channels: config.channels
@@ -14,7 +15,9 @@ bot.addListener('join#zeelbot', function(nick, message) {
   bot.say('#zeelbot', 'le zeelbot master race checking in');
 });
 
-bot.addListener('join#zeelbot', function(nick, message) {
-  bot.say('#zeelbot', 'sup ' + nick + '?');
+bot.addListener('message', function(nick, to, text, message) {
+  util.log(util.inspect(nick));
+  util.log(util.inspect(to));
+  util.log(util.inspect(text));
+  util.log(util.inspect(message));
 });
-
